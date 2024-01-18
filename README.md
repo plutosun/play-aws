62befc812535c90c040238b9473acc57e089f0ef
Pluto Sun <pluto.sun@agents1.com>
2024-01-18 14:37:04 +1100

    lambda function via hello world

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# play-aws
+# Learning Curve
+
+## Setup accounts
++ Github
++ Aws
++ openAI
+
+## Setup local lambda devevelopment env:
++ Install aws cli and register aws credentials
++ Install aws sam cli
+
+## Study how to build basic lambda via sam templates 
++ Start from helloworld ts template
++ Update event js with real my design
++ Update template.yaml to support HTTP POST only
++ Learn how to pass parameter from event request parameters
---
+++ hello-world-ts/hello-world/app.ts
@@ -0,0 +1,35 @@
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
+
+/**
+ *
+ * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
+ * @param {Object} event - API Gateway Lambda Proxy Input Format
+ *
+ * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
+ * @returns {Object} object - API Gateway Lambda Proxy Output Format
+ *
+ */
+
+export const lambdaHandler = async (event: any): Promise<APIGatewayProxyResult> => {
+    try {
+        const name = event.body.name;
+        if(!name){
+            return {
+                statusCode: 200,
+                body: "",
+            }; 
+        }
+        return {
+            statusCode: 200,
+            body: `the name is ${name}`,
+        };
+    } catch (err) {
+        console.log(err);
+        return {
+            statusCode: 500,
+            body: JSON.stringify({
+                message: 'some error happened',
+            }),
+        };
+    }
+};
