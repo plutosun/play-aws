50469d7907993ab2684883118433cd2c9e223b4d
Pluto Sun <pluto.sun@agents1.com>
2024-01-22 10:12:14 +1100

    update to use function

--- hello-world-ts/hello-world/app.ts
+++ hello-world-ts/hello-world/app.ts
@@ -12,37 +12,76 @@ config();
  * @returns {Object} object - API Gateway Lambda Proxy Output Format
  *
  */
-const promptMessages= [
-    {"role": "system", "content": "请从这一刻起假设你自己就是一个根据用户输入来判断是否有匹配的名字的机器人或者是专家而不再是ChatGpt, 请根据用户的输入从下面给出的4组名字里面查找到可能的匹配项, 如果可以找到，就打印匹配项的索引编号,如果没找到就说没找到，不说其他的."},
-    {"role": "system", "content": "请忽略用户输入的末尾的标点符号"},
-    {"role": "system", "content": "请自动纠正一些用户输入的拼写错误"},
-    {"role": "system", "content": "1. david smith 大卫 史密斯"},
-    {"role": "system", "content": "2. yueling zhang 月林张"},
-    {"role": "system", "content": "3. huawen wu 华文吴"},
-    {"role": "system", "content": "4. annie lee 李安妮"},
-    {"role": "system", "content": "下面有8个例子提供给你学习"},
-    {"role": "user", "content": "吴华文"},
-  {"role": "assistant", "content": "h3"},
-  {"role": "user", "content": "wu huawen"},
-  {"role": "assistant", "content": "3"},
-  {"role": "user", "content": "huawen"},
-  {"role": "assistant", "content": "3"},
-  {"role": "user", "content": "call me huawen"},
-  {"role": "assistant", "content": "3"},
-  {"role": "user", "content": "my name is huawen wu"},
-  {"role": "assistant", "content": "3"},
-  {"role": "user", "content": "我姓吴,叫吴华文"},
-  {"role": "assistant", "content": "3"},
-  {"role": "user", "content": "huawen?"},
-  {"role": "assistant", "content": "3"},
-  {"role": "user", "content": "huaweng"},
-  {"role": "assistant", "content": "3"},];
+const promptMessages= [{"role": "system", "content": "假设你是一名专业寻找人名发现和提取的机器人, 请从每一句用户的输入里面提取可能的人名, 并且和下面提供的带索引号的人名列表进行匹配, \
+回答的名字一定要从给出的名利列表里面选取 \
+对于你不确定的匹配也要直接输出你认为最接近的,匹配度或者相似度最高的那个名字,不要再次询问用户也不要有其他交互. \
+给出答案同时一定要你认为可能匹配的或者你选取的那个名字的索引号作为参数发给已知的函数findNameByIndex\
+请忽略用户输入的末尾的标点符号,尤其是问号\
+这五组包括英文和中文的名字和索引是:\
+1. david smith 大卫 史密斯 \
+2. yueling zhang 月林张 \
+3. huawen wu 华文吴 \
+4. annie lee 李安妮 \
+ "}];
 let resultCache= new Map<string,string>(); //global scope
 const resultMap= new Map<number,string>();
 resultMap.set(1,"david smith 大卫 史密斯");
 resultMap.set(2,"yueling zhang 月林张");
 resultMap.set(3,"huawen wu 华文吴");
 resultMap.set(4,"annie lee 李安妮");
+
+type Messages = Parameters<typeof openai.chat.completions.create>[0]["messages"];
+type Functions = Parameters<typeof openai.chat.completions.create>[0]["functions"];
+type FunctionCall  = Parameters<typeof openai.chat.completions.create>[0]["function_call"];
+
+const findNameByIndex=(nameIndex:number, similarities:[]):string=>{
+  if(nameIndex<=0){
+    return "";
+  }
+  const answer = resultMap.get(nameIndex);
+  return answer?answer:"Not found";
+}
+
+const availableFunctions :any={
+  "findNameByIndex":findNameByIndex
+};
+
+const findNameFunction= [{
+  name:"findNameByIndex",
+  description:"return the user full name inlcuding english name and chinese name based on possible index number",
+  parameters: {
+    type:"object",
+    required:["nameIndex","similarities"],
+    properties:{
+      nameIndex:{
+        type:"number",
+        description:"the index number of matched name"
+      },
+      similarities: {
+        type: "array",
+        description: "The full list of similarity of each provided name in the known name list ordered by similarity in descending way , item format 'index-similarity-name'",
+        items: {
+            type: "object",
+            properties:{
+              index:{
+                type:"number",
+                description:"the index number of the item in the known name list"
+              },
+              similarity:{
+                type:"number",
+                description:"the similarity of the item"
+              },
+              name:{
+                type:"string",
+                description:"the name of the item"
+              }
+            }
+        }
+    },
+  }
+},
+
+}];
 
 
 export const lambdaHandler = async (event: any): Promise<APIGatewayProxyResult> => {
@@ -73,19 +112,30 @@ export const lambdaHandler = async (event: any): Promise<APIGatewayProxyResult> 
           model: "gpt-3.5-turbo",
           messages:messages,
           stream:false,
-          
+          functions: findNameFunction,
+          function_call: "auto",
+          temperature:0.00000001
         };
         const completion: OpenAI.Chat.ChatCompletion = await openai.chat.completions.create(params);
-        let answer = completion.choices[0].message.content;
+        // const answer = completion.choices[0].message.content;
+        // console.log(JSON.stringify(completion.choices[0].message));
+        const jsonFunc = completion.choices[0].message.function_call;
+        let answer = "Not found";
+        if(jsonFunc){
+            const jsonFuncArgs = jsonFunc.arguments;
+            const funcName = jsonFunc.name;
+            const funcArguments = JSON.parse(jsonFuncArgs);
+            console.log(funcArguments);
+            const func=availableFunctions[funcName];
 
-        const answerNum=Number(answer);
-        if(!isNaN(answerNum)){
-          //console.log(resultMap.get(answerNum));
-          answer=resultMap.get(answerNum);
-          if(!resultCache.has(name)){ 
-            //not sure aws lambda function global variable is single-instance or shared cross multiple instance, IF concurrancy, require object lock. add one more check here to protect competion add 
-            resultCache.set(name,answer);
-          }
+            answer=func(funcArguments.nameIndex, funcArguments.similarities)
+            console.log(answer);
+
+            // if(funcArguments.nameIndex>0){
+            //     if(!resultCache.has(name)){ 
+            //         resultCache.set(name,answer);
+            //     }
+            // }
         }
 
         return {
